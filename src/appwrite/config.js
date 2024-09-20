c183868b32cbde4e6c7198f0b127131f2a12420e
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-09-20 19:09:11 +0530

    worked on getpost(s)

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,6 +1,35 @@
 import conf from "../conf/conf"
 import {Client, Databases, Storage, Query } from "appwrite"
 
+export class Service{
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor(){
+        this.client.setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.bucket = new Storage(this.client)
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
+        } catch(error){
+            console.log("Appwrite service :: getPost() ::", error)
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try{
+            return await this.databases.listDocuments(conf.appwriteDatabaseId, conf.appwriteCollectionId, queries)
+        } catch(error){
+            console.log("Appwrite service :: getPosts() ::", error)
+        }
+    }
+}
+
 const client = new Client()
 
 const databases = new Databases(client);
