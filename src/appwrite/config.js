c00ff403db822928ac8ab0bbbfe2d5f4b1ddd7c2
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-09-20 19:22:06 +0530

    added code to createPost

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -18,6 +18,7 @@ export class Service{
             return await this.databases.getDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug)
         } catch(error){
             console.log("Appwrite service :: getPost() ::", error)
+            return false
         }
     }
 
@@ -26,6 +27,23 @@ export class Service{
             return await this.databases.listDocuments(conf.appwriteDatabaseId, conf.appwriteCollectionId, queries)
         } catch(error){
             console.log("Appwrite service :: getPosts() ::", error)
+            return false
+        }
+    }
+    
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title, content, featuredImage, status, userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost() ::", error)
+            return false
         }
     }
 }
@@ -36,5 +54,5 @@ const databases = new Databases(client);
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('');
+    .setProject('66e59776000ad65b941c');
 
